7b45d473c4add6a080d5eb937812f22f75512828
Nastya Tsimanovich <nastya.cimanovich.97@gmail.com>
2019-10-04 10:37:10 +0300

    feat: add solution for 80% test

--- src/index.js
+++ src/index.js
@@ -1,112 +1,136 @@
 module.exports = function zeros(expression) {
   // your solution
-  let arr=expression.split('!!').join('');
-  let arr_num=[];
-  let arr_num1=[];
-  let arr_num2=[];
   let count=0;
+  let factorial1=[];
+  let factorial2=[];
+  let factorial3=[];
   let even=0;
   let odd=0;
+  let count2=0;
+  let count5=0;
+  if(expression.indexOf('!!')!=-1){
+    factorial2=expression.split('!!').join('');
+    console.log(factorial2);
+    //если в факториале все двойные
+      if(factorial2.indexOf('!')==-1){
+        factorial2=factorial2.split('*');
+        for (let i=0;i<factorial2.length;i++){
+          factorial2[i]=+factorial2[i];
+          if(factorial2[i]%2==0){
+            even++;
+          }
+          else{
+            odd++;
+          }
+        }
+        for(let i=0;i<factorial2.length;i++){
+          //если все четные
+          if(odd==0){
+            let k=10;
+            count+=Math.floor(factorial2[i]/k);
+            count+=Math.floor(factorial2[i]/50);
+          }
+          console.log(factorial2);
+          //если все нечетные
+          if(even==0){
+            count+=0;
+          }
+          //если и четные, и не четные
+          if(even>0 && odd>0){
+            let k=5;
+            while(factorial2[i]>=k){
+              count+=Math.floor(factorial2[i]/k);
+              k*=5;
+            }
+            let m=10;
+            while(factorial2[i]>=m){
+              count-=Math.floor(factorial2[i]/m);
+              m*=5;
+            }  
+          } 
+        }
+        console.log(count);
+      }
 
-  console.log(arr);
-  // arr_num=arr.match(/\d+\*/g);
-  // console.log(arr_num);
-  
-    if(arr.indexOf('!')!=-1){
-      arr_num1=arr.match(/\d+!/g);
-        for(let j=0; j<arr_num1.length; j++){
-          arr_num1[j]=parseInt(arr_num1[j]);       
+    //если есть и двойные и одичные
+      if(factorial2.indexOf('!')!=-1){
+        factorial1=factorial2.match(/\d+!/g);
+        factorial2=factorial2.replace(/\d+!/g,'*').split('*');
+
+      //если одиночный
+      for(let j=0; j<factorial1.length; j++){
+        factorial1[j]=parseInt(factorial1[j]);       
+        let k=5;
+        while(factorial1[j]>=k){
+          count+=Math.floor(factorial1[j]/k);
+          k*=5;  
+        }
+      }
+
+      console.log(factorial1);
+      console.log(count);     
+      // если двойной
+        //двойной четный
+        //двойной нечетный
+        //и четные, и нечетный
+      for (let i=0;i<factorial2.length;i++){
+        if(+factorial2[i]>0){
+          factorial3.push(+factorial2[i]);
+        }
+      }
+      console.log(factorial3);
+      
+      for(let i=0;i<factorial3.length;i++){
+                   
+            if(factorial3[i]%2==0){
+              let m=2;
+              while(factorial3[i]>m){
+                count2+=Math.floor(factorial3[i]/m);
+                m*=2;
+              }
+            }
+            if(factorial3[i]%2==0 ){
+              
+              count5+=Math.floor(factorial3[i]/10);
+              count5+=Math.floor(factorial3[i]/50);
+            }  
+            
+            if(factorial3[i]%2!=0){
+              let l=5;
+              while(factorial3[i]>=l){
+                count5+=Math.floor(factorial3[i]/l);
+                l*=5;
+              }
+              count5-=Math.floor(factorial3[i]/10);
+              count5-=Math.floor(factorial3[i]/50);
+            }  
+            
+            } 
+            console.log(count2);
+            console.log(count5);
+            if(count2>count5){
+              count+=count5;
+            } 
+            
+            if(count2<=count5 && count2==0){
+            count+=count5;
+            }              
+    }
+     
+  }
+  else{
+    factorial1=expression.match(/\d+!/g);
+    console.log(factorial1);
+        for(let j=0; j<factorial1.length; j++){
+          factorial1[j]=parseInt(factorial1[j]);       
           let k=5;
-          while(arr_num1[j]>=k){
-            count+=Math.floor(arr_num1[j]/k);
+          while(factorial1[j]>=k){
+            count+=Math.floor(factorial1[j]/k);
             k*=5;  
           }
         }
-    }    
-    console.log(arr_num1);
-    
-
-  if(arr.indexOf('!')==-1){
-      arr_num2=arr.split('*');
-      for(let i=0; i<arr_num2.length;i++){
-        arr_num2[i]=+arr_num2[i];
-        if(arr_num2[i]%2==0){
-          even+=1;
-        }
-        else{
-          odd+=1;
-        }
-      }
-      for(let j=0;j<arr_num2.length;j++){
-        if(odd==0 && arr_num2[j]>10){
-          let k=10;
-          count+=Math.floor(arr_num2[j]/k);  
-          count+=Math.floor(arr_num2[j]/50);        
-        }
-        
-        if(odd==0 && arr_num2[j]==10){
-          let k=10;
-          count+=Math.floor(arr_num2[j]/k);  
-        }
-        if(even>0 && odd>0){
-          if(arr_num2[j]%2!=0){
-            let k=5;
-            while(arr_num2[j]>=k){
-              count+=Math.floor(arr_num2[j]/k);
-              k*=5;  
-            }
-            count-=Math.floor(arr_num2[j]/10);
-          }
-          else{
-            let k=10;
-            count+=Math.floor(arr_num2[j]/k);
-          } 
-        }
-      }
-
+    console.log(count);    
   }
-    
-    console.log(arr_num2);
-    console.log(count);
-    console.log(even,odd);
-
-
-
-
-
-  // arr_num1=(expression.match(/\d+!/g));
-  // console.log(arr_num1);
-
-  // for(let i=0; i<arr_num1.length;i++){
-  //   arr_num1[i]=arr_num1[i].split('!').join('');
-  //   arr_num1[i]=+arr_num1[i];
-  // }
-  // console.log(arr_num1);
-
-  // arr_num2.push(expression.match(/\d+!!/g));
-  // console.log(arr_num2);
-
-  // for(let i=0;i<arr.length;i++){
-  //   if(Number.isInteger(+arr[i]) && !Number.isInteger(+arr[i+1]) && !Number.isInteger(+arr[i-1]) /*&& arr[i+2]!='!'*/){
-  //     arr_num.push(+arr[i]);
-  //     while(+arr[i]>=j){
-  //       count+=Math.floor(+arr[i]/5);
-  //       j*=5;
-  //     }
-  //   }
-  //     if(Number.isInteger(+arr[i]) && Number.isInteger(+arr[i+1]) && !Number.isInteger(+arr[i+2])){
-  //       //if(+arr[i]%2==0 && +arr[i]>=10){
-  //         arr[i]=+(arr[i]+arr[i+1]);
-  //         arr_num.push(+arr[i]);
-  //         // while(+arr[i]>=j){
-  //         //   count+=Math.floor(+arr[i]/5);
-  //         //   j*=5;
-  //         // }
-  //      // }
-  //     }
-    
-  //   // if(Number.isInteger(+arr[i]) && !Number.isInteger(+arr[i+1])&& !Number.isInteger(+arr[i-1])){
-  //   //   arr_num.push(+arr[i]);
-  //   // }
-  return count;
+  console.log(count);
+return count;
 }
